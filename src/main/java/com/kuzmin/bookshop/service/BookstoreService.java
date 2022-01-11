bd2bae08a9f787afd1ed8d3a3c11de967fcfd6d3
kuzminal <kuzmin.al35@gmail.com>
2022-01-11 18:17:30 +0300

    Добавил скрипт по заливке данных. Убрал инициализацию данных из сервиса.

--- src/main/java/com/kuzmin/bookshop/service/BookstoreService.java
+++ src/main/java/com/kuzmin/bookshop/service/BookstoreService.java
@@ -1,60 +1,18 @@
 package com.kuzmin.bookshop.service;
 
-import com.kuzmin.bookshop.entity.Author;
 import com.kuzmin.bookshop.entity.Book;
-import com.kuzmin.bookshop.repository.AuthorRepository;
 import com.kuzmin.bookshop.repository.BookRepository;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 @Service
 public class BookstoreService {
-
-    private final AuthorRepository authorRepository;
     private final BookRepository bookRepository;
 
-    public BookstoreService(AuthorRepository authorRepository, BookRepository bookRepository) {
-
-        this.authorRepository = authorRepository;
+    public BookstoreService(BookRepository bookRepository) {
         this.bookRepository = bookRepository;
-        insertAuthorWithBooks();
         getAll();
-    }
-
-    @Transactional
-    public void insertAuthorWithBooks() {
-        Author jn = new Author();
-        jn.setName("Joana Nimar");
-        jn.setAge(34);
-        jn.setGenre("History");
-        Book jn01 = new Book();
-        jn01.setIsbn("001-JN");
-        jn01.setTitle("A History of Ancient Prague");
-        Book jn02 = new Book();
-        jn02.setIsbn("002-JN");
-        jn02.setTitle("A People's History");
-        Book jn03 = new Book();
-        jn03.setIsbn("003-JN");
-        jn03.setTitle("World History");
-        jn.addBook(jn01);
-        jn.addBook(jn02);
-        jn.addBook(jn03);
-        authorRepository.save(jn);
-
-        Author author = new Author();
-        author.setName("Alicia Tom");
-        author.setAge(38);
-        author.setGenre("Anthology");
-
-        Book book = new Book();
-        book.setIsbn("001-AT");
-        book.setTitle("The book of swords");
-
-        author.addBook(book); // use addBook() helper
-
-        authorRepository.save(author);
     }
 
     List<Book> getAll() {
---
+++ src/main/resources/data.sql
@@ -0,0 +1,24 @@
+Delete from bookstore.author_book;
+Delete from bookstore.author;
+Delete from bookstore.book;
+Delete from bookstore.publisher;
+
+
+INSERT into bookstore.author (id, age, genre, name)
+values (1, 34, 'History', 'Joana Nimar');
+INSERT into bookstore.author (id, age, genre, name)
+values (2, 38, 'Anthology', 'Alicia Tom');
+
+insert into bookstore.publisher (id, company)
+values (1, 'Company 1');
+
+insert into bookstore.book (id, isbn, title, publisher_id)
+values (1, '001-JN', 'A History of Ancient Prague', 1);
+
+insert into bookstore.book (id, isbn, title, publisher_id)
+values (2, '002-JN', 'A Peoples History', 1);
+
+insert into bookstore.author_book (author_id, book_id)
+values (1, 1);
+insert into bookstore.author_book (author_id, book_id)
+values (2, 2);
